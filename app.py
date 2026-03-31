6fce278a61dad88725095dbc12033507e7ec7748
Dharamveer06 <dharamveervadoliya@gmail.com>
2026-03-31 13:35:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 
 # Load model
 model = load_model('emotion_model.h5')

ef15d09a312bf5a14d71f71978fedcb53c952d65
Dharamveer06 <dharamveervadoliya@gmail.com>
2026-08-10 12:05:23 +0530

    Refactor emotion detection app with Streamlit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 from transformers import pipeline
 
-st.set\_page\_config(page\_title="Emotion Detector", layout="centered")
+st.set-page-config(page-title="Emotion Detector", layout="centered")
 
 st.title("😊 Face Emotion Detector + Mood Booster")
 st.write("Simple & Clean Emotion Detection")
